de2643f1261610f1e39f8cb82858087d073a17f0
samsul kobir <makkobir79@gmail.com>
2023-11-26 05:26:55 +0200

    first user created

---
+++ src/gql/resolver/index.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const resolvers = {
+  Query: {},
+
+  Mutation: {
+    signup: async (parent: any, args: any, context: any) => {
+      return await prisma.user.create({
+        data: args,
+      });
+    },
+  },
+};
